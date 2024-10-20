354af4d4fcc72aa4cae628234c13d706c25b623c
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2024-10-20 12:29:22 +0200

    Add files via upload

---
+++ SAE301-master/api/Controller/CartController.php
@@ -0,0 +1,59 @@
+<?php
+require_once "Controller.php";
+require_once "Repository/CartRepository.php" ;
+
+class CartController extends Controller {
+
+    private CartRepository $cart;
+
+    public function __construct(){
+        $this->cart = new CartRepository();
+    }
+
+   
+    protected function processGetRequest(HttpRequest $request) {
+        $id = $request->getId("id");
+        $id2 = $request->getId2("id2");
+        $id3 = $request->getId3("id3");
+        if ($id){
+            if($id == "add"){
+                if($id2){
+                    if($id3){
+                        $p = $this->cart->save($id2, $id3);
+                        return $p==null ? false :  $p;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+            if($id == "del"){
+                if($id2){
+                    if($id3){
+                        $p = $this->cart->delete($id2, $id3);
+                        return $p==null ? false :  $p;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                $p = $this->cart->find($id);
+                return $p==null ? false :  $p;
+            }
+        }
+    }
+
+    protected function processPostRequest(HttpRequest $request) {
+        return false;
+    }
+   
+}
+?>
---
+++ SAE301-master/api/Repository/CategoryRepository.php
@@ -0,0 +1,90 @@
+<?php
+
+require_once("Repository/EntityRepository.php");
+require_once("Class/Product.php");
+require_once("Class/Short.php");
+require_once("Class/Tshirt.php");
+require_once("Class/Chaussures.php");
+require_once("Class/Type.php");
+
+class CategoryRepository extends EntityRepository {
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public static function createProduct($data){
+        $classname = ucfirst($data["type"]);
+        if (class_exists($classname)){
+            return new $classname($data);
+        }
+        else{
+            throw new Exception("Type $classname inconnu");
+        }
+    }
+
+    public function find($type): array{
+        $requete = $this->cnx->prepare("select * from Products where type=:value");
+        $requete->bindParam(':value', $type);
+        $requete->execute();
+
+        $answer = $requete->fetchAll(PDO::FETCH_OBJ);
+
+        $res = [];
+        
+        foreach($answer as $obj){
+            $p = self::createProduct((array)$obj);
+            $p->setId($obj->id_product);
+            $p->setName($obj->name);
+            $p->setSubtitle($obj->subtitle);
+            $p->setDescription($obj->description);
+            $p->setType($obj->type);
+            $p->setPrice($obj->price);
+            $p->setImage($obj->image);
+            $p->setGender($obj->gender);
+            $p->setSize($obj->size);
+            $p->setShoesize($obj->shoesize);
+            $p->setColor($obj->color);
+            $p->setStock($obj->stock);
+            array_push($res, $p);
+        }
+       
+        return $res;
+    }
+    
+
+    public function findType(): array {
+        $requete = $this->cnx->prepare("select distinct type from Products");
+        $requete->execute();
+        $answer = $requete->fetchAll(PDO::FETCH_OBJ);
+
+        $res = [];
+        
+        foreach($answer as $obj){
+            $p =  new Type((array)$obj);
+            $p->setType($obj->type);
+            array_push($res, $p);
+        }
+       
+        return $res;
+    }
+
+    public function findAll(){
+        return false;
+    }
+
+    public function save($product,$product2){
+        return false;
+    }
+
+    public function delete($product,$product2){
+        return false;
+    }
+
+    public function update($product){
+        return false;
+    }
+
+   
+    
+}
