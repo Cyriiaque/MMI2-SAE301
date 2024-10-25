9abe8b61e4b70640215382d1778b81325d3a25f7
Lemesle Cyriaque <100539151+Cyriiaque@users.noreply.github.com>
2024-10-25 17:06:10 +0200

    Delete SAE301-master/client directory

--- SAE301-master/client/src/data/user.js
+++
@@ -1,8 +0,0 @@
-import {postRequest} from '../lib/api-request.js';
-
-let UserData = {};
-
-UserData.signup = async function (formdata) {
-    let data = await postRequest('users/signup', formdata);
-    return data;
-}
